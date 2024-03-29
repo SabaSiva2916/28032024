1bd4363d03e6aa2f94aacba723fd43465e712c38
Sabasiva2916 <sabasiva2916@gmail.com>
2024-03-29 18:51:32 +0530

    Work done

---
+++ src/org/git/GitPush.java
@@ -0,0 +1,14 @@
+package org.git;
+//pushing these project from Local to Remote
+public class GitPush {
+
+    public static void main(String[] args) {
+
+	System.out.println("Working Directory");
+	System.out.println("Stagging");
+	System.out.println("Local Git Repository");
+	System.out.println("Remote Git Repository");
+	
+    }
+
+}
