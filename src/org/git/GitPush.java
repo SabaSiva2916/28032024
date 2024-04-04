c03b5624429b39d834f2d101c30c14a898ebf372
Sabasiva2916 <sabasiva2916@gmail.com>
2024-04-04 19:01:33 +0530

    B works

--- src/org/git/GitPush.java
+++ src/org/git/GitPush.java
@@ -1,4 +1,8 @@
 package org.git;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //pushing these project from Local to Remote
 public class GitPush {
 
@@ -8,7 +12,19 @@ public class GitPush {
 	System.out.println("Stagging");
 	System.out.println("Local Git Repository");
 	System.out.println("Remote Git Repository");
-	
+	// creating List with String type and adding value
+	List<String> li = new ArrayList<>();
+	// Adding vlaue to list --> add()
+	li.add("A");
+	li.add("B");
+	li.add("C");
+	li.add("D");
+	li.add("E");
+
+	// Iterating the list value by using for loop
+	for (int i = 0; i < li.size(); i++) {
+	    System.out.println(li.get(i));
+	}
     }
 
 }
